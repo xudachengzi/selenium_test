8a6d72119962a1900e556c257fb163f0d7188f03
SC-201812191507\Administrator <xu649047654@163.com>
2019-03-19 17:23:55 +0800

    iframe

--- test/find_element/operat_iframe.py
+++ test/find_element/operat_iframe.py
@@ -4,13 +4,18 @@ from selenium import webdriver
 
 
 # 获取当前窗口句柄
-def test_handle(driver):
-    driver.get("http://bk.ganji.com")
-    driver.implicitly_wait(10)
-    h = driver.current_window_handle  # 获取当前窗口句柄 h是一个集合
+def test_iframe(driver):
+    driver.get("http://mail.163.com")
+    driver.implicitly_wait(30)
+    # 切换iframe
+    iframe = driver.find_element_by_tag_name("iframe")
+    driver.switch_to_frame(iframe)
+    driver.find_element_by_name("email").send_keys("123")
+    driver.find_element_by_name("password").send_keys("456")
+
 
 if __name__ == '__main__':
     driver = webdriver.Firefox()
-    test_handle(driver)
+    test_iframe(driver)
     time.sleep(2)
     driver.quit()
